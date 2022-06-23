4fcdf4d2dfe82ff0ad65988ea64e974d42682f7b
D0hwQ1 <pjt137@naver.com>
2022-06-23 16:33:37 +0900

    Add(main.go): total manage program

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/D0hwQ1/Blockchain-With-Go/p2p"
+	"github.com/D0hwQ1/Blockchain-With-Go/pos"
+	"github.com/D0hwQ1/Blockchain-With-Go/pow"
+	"github.com/D0hwQ1/Blockchain-With-Go/tcp"
+	"github.com/D0hwQ1/Blockchain-With-Go/web"
+)
+
+func main() {
+	var port int = 0
+
+	for {
+		fmt.Print("사용할 포트 입력: ")
+		fmt.Scanf("%d", &port)
+
+		if port != 0 {
+			fmt.Println()
+			break
+		} else {
+			fmt.Println("\n잘못된 포트 입력입니다.\n다시 입력해주세요\n")
+			continue
+		}
+	}
+
+	fmt.Println("web : 블록체인 웹서비스를 구동합니다.")
+	fmt.Println("tcp : 블록체인 tcp통신을 구동합니다.")
+	fmt.Println("pow : PoW 합의 알고리즘 방식의 블록체인 웹서비스를 구동합니다.")
+	fmt.Println("pos : PoS 합의 알고리즘 방식의 블록체인 웹서비스를 구동합니다.")
+	fmt.Println("p2p : 중앙 노드 기반의 블록체인 웹서비스를 구동합니다.\n\n")
+
+	for {
+		var name string
+
+		fmt.Print("수행할 작업 입력: ")
+		fmt.Scanf("%s", &name)
+		fmt.Println()
+
+		switch name {
+		case "web":
+			fmt.Println("링크: http://localhost:" + string(port))
+			fmt.Println("블록을 생성하실 때에는, 링크에 POST 방식으로 {BPM: value(num)}를 입력하시면 됩니다\n")
+			web.Start()
+		case "tcp":
+			fmt.Println("접속: nc localhost", port, "\n")
+			tcp.Start()
+		case "pow":
+			fmt.Println("링크: http://localhost:" + string(port))
+			fmt.Println("블록을 생성하실 때에는, 링크에 POST 방식으로 {BPM: value(num)}를 입력하시면 됩니다\n")
+			pow.Start()
+		case "pos":
+			fmt.Println("접속: nc localhost", port, "\n")
+			pos.Start()
+		case "p2p":
+			var yn string
+
+			fmt.Print("노드 접속(y/n): ")
+			fmt.Scanf("%s", &yn)
+
+			if strings.ToLower(yn) == "y" || strings.ToLower(yn) == "yes" {
+				fmt.Print("주소 입력: ")
+				fmt.Scanf("%s", &yn)
+				p2p.Start(port, true, yn)
+			} else {
+				fmt.Print("secio 적용(y/n): ")
+				fmt.Scanf("%s", &yn)
+				fmt.Println()
+
+				if strings.ToLower(yn) == "y" || strings.ToLower(yn) == "yes" {
+					p2p.Start(port, true, "")
+				} else {
+					p2p.Start(port, false, "")
+				}
+			}
+
+		default:
+			fmt.Printf("'%s'은 잘못된 입력입니다.\n\n\n", name)
+		}
+	}
+}
